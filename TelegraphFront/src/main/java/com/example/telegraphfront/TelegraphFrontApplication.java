8a8155cd955dd9beae2b64c1bd271232ef536026
Javokhir  Jakhonov <84447794+javohirjahonov@users.noreply.github.com>
2023-05-25 09:44:14 +0500

    Add files via upload

---
+++ TelegraphFront/src/main/java/com/example/telegraphfront/TelegraphFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegraphfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegraphFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegraphFrontApplication.class, args);
+    }
+
+}
---
+++ TelegraphFront/src/test/java/com/example/telegraphfront/TelegraphFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegraphfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegraphFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
